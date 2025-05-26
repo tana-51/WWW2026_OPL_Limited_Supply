b2a478eed7017ac135b308fc3956d680f222df32
tana-51 <tianzhonghuangyi006@gmail.com>
2025-05-26 19:08:34 +0900

    rename

---
+++ run.sh
@@ -0,0 +1,11 @@
+#lambda
+# python ./src/main_lambda.py setting.run_file="main_lambda" 
+
+#heatmap
+# python ./src/main_heatmap.py setting.run_file="main_heatmap" 
+
+#supply item
+# python ./src/main_supply_item.py setting.run_file="main_supply_item" 
+
+#user_action_ratio
+python ./src/main_user_action_ratio.py setting.run_file="main_user_action_ratio" 
